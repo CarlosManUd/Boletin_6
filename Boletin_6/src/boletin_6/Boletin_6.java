307ffb9ef4e52763f9b3476f84c9a88dc1f4fb7e
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-10-29 09:38:28 +0100

    Boletin6_1

---
+++ Boletin_6/src/boletin_6/Boletin_6.java
@@ -0,0 +1,17 @@
+package boletin_6;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin_6 {
+
+    
+    public static void main(String[] args) {
+       
+        Numero num = new Numero();
+        num.positivo();
+        
+    }
+    
+}
